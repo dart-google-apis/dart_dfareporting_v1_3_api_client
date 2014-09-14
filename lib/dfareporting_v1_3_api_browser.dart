d7d209462b993dbaf73d3c3c06081adc5a9323d5
Adam Singer <financeCoding@gmail.com>
2014-09-13 18:51:50 -0700

    Automated update

--- lib/dfareporting_v1_3_api_browser.dart
+++ lib/dfareporting_v1_3_api_browser.dart
@@ -6,6 +6,7 @@ import 'package:google_dfareporting_v1_3_api/src/browser_client.dart';
 import "package:google_dfareporting_v1_3_api/dfareporting_v1_3_api_client.dart";
 
 /** Lets you create, run and download reports. */
+@deprecated
 class Dfareporting extends Client with BrowserClient {
 
   /** OAuth Scope2: View and manage DoubleClick for Advertisers reports */
